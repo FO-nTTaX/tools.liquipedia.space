62b9ba31eeb003b993f85a1febd41b16fccac40f
FO-nTTaX <fo.nttax@yahoo.de>
2018-09-06 21:56:00 +0200

    naughty list

--- view/discord.php
+++ view/discord.php
@@ -1,18 +1,19 @@
 <?php
 
 $db = new PDO( 'mysql:dbname=liquipedia_discord;host=localhost', $CONFIG[ 'db' ][ 'user' ], $CONFIG[ 'db' ][ 'password' ] );
-$res = $db->prepare( 'SELECT `user`, SUM(`counter`) as `count` FROM `liquipedia_discord` GROUP BY `user_id` ORDER BY `count` DESC LIMIT 500' );
+$res = $db->prepare( 'SELECT `user`, SUM(`counter`) as `count`, SUM(`naughtycounter`) as `naughtycount` FROM `liquipedia_discord` GROUP BY `user_id` ORDER BY `count` DESC LIMIT 500' );
 $res->execute();
 $text = '';
 $text .= '<h2>Liquipedia Discord spammers</h2>';
 $text .= '<table>';
-$text .= '<tr><th>#</th><th>User</th><th>Count</th></tr>';
+$text .= '<tr><th>#</th><th>User</th><th>Count</th><th>Naughty Count</th></tr>';
 $i = 0;
 while ( $row = $res->fetchObject() ) {
 	$text .= '<tr>';
 	$text .= '<th>' . ( ++$i ) . '</th>';
 	$text .= '<td>' . $row->user . '</td>';
 	$text .= '<td>' . $row->count . '</td>';
+	$text .= '<td>' . $row->naughtycount . '</td>';
 	$text .= '</tr>';
 }
 $text .= '</table>';
