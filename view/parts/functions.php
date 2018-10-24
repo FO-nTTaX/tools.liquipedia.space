c8722ba71226632f7e8462d2607dbceb26a65a8b
FO-nTTaX <fo.nttax@yahoo.de>
2018-10-24 16:02:06 +0200

    style

--- view/parts/functions.php
+++ view/parts/functions.php
@@ -4,7 +4,7 @@ function file_get_contents_gzip( $path ) {
 	global $CONFIG;
 	$content = file_get_contents( $path, false, $CONFIG[ 'httpcontext' ] );
 	foreach ( $http_response_header as $c => $h ) {
-		if ( stristr( $h, 'content-encoding' ) and stristr( $h, 'gzip' ) ) {
+		if ( stristr( $h, 'content-encoding' ) && stristr( $h, 'gzip' ) ) {
 			$content = gzinflate( substr( $content, 10, -8 ) );
 		}
 	}
