c0c6de28f0dafecc215f70940f85a3e422d5d3d3
FO-nTTaX <fo.nttax@yahoo.de>
2018-11-29 12:16:36 +0100

    added timezone detector

--- view/timezonedetect.php
+++ view/timezonedetect.php
@@ -4,7 +4,7 @@ $text = '';
 
 $text .= '<input type="text" id="detectedtimezone" readonly>';
 $text .= '<script>';
-$text .= 'var result;var parts = ( new Intl.DateTimeFormat( \'en\', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, timeZoneName: \'long\' } ) ).formatToParts();';
-$text .= 'parts.forEach( function( el ) {if ( el.type === \'timeZoneName\' ) {result = el.value;}} );';
-$text .= 'document.getElementById(\'detectedtimezone\').value = result;';
+$text .= 'var result; var parts = ( new Intl.DateTimeFormat( \'en\', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, timeZoneName: \'long\' } ) ).formatToParts();';
+$text .= 'parts.forEach( function( el ) { if ( el.type === \'timeZoneName\' ) { result = el.value; } } );';
+$text .= 'document.getElementById( \'detectedtimezone\' ).value = result;';
 $text .= '</script>';
