f729256fa2b5e783643349d2e635b0b567e0551c
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2021-03-22 23:32:04 +0100

    Update jobs.php

--- view/jobs.php
+++ view/jobs.php
@@ -1,18 +1,32 @@
 <?php
 
 $CONFIG[ 'wikis' ] = json_decode( file_get_contents_gzip( $CONFIG[ 'wikibaseurl' ] . '/api.php?action=listwikis' ), true );
-ksort( $CONFIG[ 'wikis' ][ 'allwikis' ] );
+
+$aWikiData = [];
+foreach ( $CONFIG[ 'wikis' ][ 'allwikis' ] as $sWiki => $aWiki ) {
+	$sData = file_get_contents_gzip( $aWiki[ 'api' ] . '?action=query&meta=siteinfo&formatversion=2&format=json&siprop=statistics' );
+	$aData = json_decode( $sData, true );
+	$aWikiData[] = [
+		'name' => $aWiki[ 'name' ],
+		'jobs' => intval( $aData[ 'query' ][ 'statistics' ][ 'jobs' ] ),
+	];
+}
+
+usort( $aWikiData,function( $a, $b ) {
+	if ( $a[ 'jobs' ] === $b[ 'jobs' ] ) {
+		return $a[ 'name' ] > $b[ 'name' ] ? -1 : 1;
+	}
+	return $a[ 'jobs' ] > $b[ 'jobs' ] ? -1 : 1;
+} );
 
 $text = '';
 
 $text .= '<div class="table-responsive">';
 $text .= '<table>';
-foreach ( $CONFIG[ 'wikis' ][ 'allwikis' ] as $sWiki => $aWiki ) {
+foreach ( $aWikiData as $aData ) {
 	$text .= '<tr>';
-	$text .= '<th>' . $aWiki[ 'name' ] . '</th>';
-	$sData = file_get_contents_gzip( $aWiki[ 'api' ] . '?action=query&meta=siteinfo&formatversion=2&format=json&siprop=statistics' );
-	$aData = json_decode( $sData, true );
-	$text .= '<td>' . $aData[ 'query' ][ 'statistics' ][ 'jobs' ] . '</td>';
+	$text .= '<th>' . $aData[ 'name' ] . '</th>';
+	$text .= '<td>' . $aData[ 'jobs' ] . '</td>';
 	$text .= '</tr>';
 }
 $text .= '</table>';
