42a62c095cf5d8d618c1542830acabe142cc5afd
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2024-12-14 19:03:14 +0100

    fix page URL

--- view/parts/footer.php
+++ view/parts/footer.php
@@ -1,3 +1,3 @@
 <?php
 
-$footer = '<div class="footer">liquipedia.tools &copy; 2016-' . date( 'Y' ) . ' &mdash; <a href="https://fo-nttax.de" target="_blank">FO-nTTaX</a>, <a href="https://liquipedia.space" target="_blank">liquipedia.space</a></div>';
+$footer = '<div class="footer">liquipedia.tools &copy; 2016-' . date( 'Y' ) . ' &mdash; <a href="https://fo-nttax.de" target="_blank">FO-nTTaX</a>, <a href="https://liquipedia.0xff0000.dev" target="_blank">liquipedia.0xff0000.dev</a></div>';
